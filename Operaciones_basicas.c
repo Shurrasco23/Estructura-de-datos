d579b63d6854b0d607b33568ea8ef86245aec6dd
Shurrasco23 <alcarras@umag.cl>
2025-09-13 20:20:16 -0300

    Ejemplo 3
    Porgrama que recibe 3 parametros numericos y hace las 4 opreaciones basicas con ellos

---
+++ Operaciones_basicas.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+
+{   
+	//el programa solo aceptara un maximo de 2 parametros 
+    if (argc > 4) {
+        printf("Usage %s: <arg1><arg2> \n", argv[0]);
+        return 1;
+    }
+    
+    // se utiliza atoi para cambiar los parametros a enteros antes se debe incluir stdlib
+
+    int num = atoi(argv[1]);
+    int numB = atoi(argv[2]);
+    int numC = atoi(argv[3]);
+
+    int suma = num + numB + numC;
+    int resta = num - numB + numC;
+    int multiplicacion = num * numB * numC;
+    
+    printf("suma: %d\n", suma);
+    printf("resta: %d\n", resta);
+    printf("multiplicacion %d\n", multiplicacion);
+	
+    //division
+	
+    	if (numB == 0 || numC == 0)
+	printf ("ERROR: no se puede dividir por 0"); //si el segundo parametro es 0 imprime ERROR
+	
+	else {
+		
+		float division= num / numB / numC;
+		printf ("division: %.5f\n", division);
+	
+	}
+
+    return 0;
+}
